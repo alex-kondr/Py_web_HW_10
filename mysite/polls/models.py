4708570778bfab6b0dd5c616ded87a5c4514fdd1
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2023-02-28 16:55:13 +0200

    іфму

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import dateutil
 
 
 # Create your models here.
@@ -10,3 +11,18 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    
+    
+class Author(models.Model):
+    
+    fullname = models.CharField(max_length=200)
+    born_date = models.DateTimeField(default=dateutil.parser)
+    born_location = models.CharField()
+    description = models.CharField()
+    
+
+class Quote(models.Model):
+    
+    tags = models.()
+    author = ReferenceField(Author)
+    quote = models.CharField()
