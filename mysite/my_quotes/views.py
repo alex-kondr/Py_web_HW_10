082c65fd080adaf662f564e0226800a2800ca9d9
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2023-03-02 16:58:58 +0200

    save

--- mysite/my_quotes/views.py
+++ mysite/my_quotes/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Author, Quote
 
 
 # Create your views here.
 def index(request):
     
+    quotes = Quote.objects.all()
+    
     return render(request, 
                   "my_quotes/index.html", 
-                  context={"title": "My quotes site"})
+                  context={"quotes": quotes})
 
 
