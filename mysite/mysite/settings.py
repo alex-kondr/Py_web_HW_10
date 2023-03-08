8ae08242d3c1afead935eb43f8730c390992831f
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2023-03-08 14:19:35 +0200

    save

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,9 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'polls',
     'my_quotes',
-    'test_images',
     'users'
 ]
 
